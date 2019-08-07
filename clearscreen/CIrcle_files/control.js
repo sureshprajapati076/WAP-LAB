7df2cfa1ca4b773bd54837242f88175016dcec70
sureshprajapati076 <su.prajapati@mum.edu>
2019-08-06 23:09:28 -0500

    q

--- clearscreen/CIrcle_files/control.js
+++ clearscreen/CIrcle_files/control.js
@@ -2,19 +2,21 @@ $(function(){
 
 	"use strict";
 	let timerId=null;
-
+	let timerForOpacity=null;
 	
 	
 	$('#start').click(showCircle);
 	
 	
 	function showCircle(){
-		
+
+
+		$('#result').text('Pop Out All Circles');
 		let width=$('#width').val();
 	
 		let num=$('#number').val();
-		
-		
+
+		$('.circle').remove();
 		$('.circle').show().css({"height":width,"width":width,"top":"200px","left":"50%"});
 
 		let count=parseInt($('#number').val());
@@ -24,6 +26,8 @@ $(function(){
 		$newCircle.addClass("circle");
 		let col="rgb("+Math.floor(Math.random() * 255)+","+Math.floor(Math.random() * 255)+","+Math.floor(Math.random() * 255) +")";
 		$newCircle.css("background-color",col);
+		$newCircle.css("top",Math.random()*200);
+			$newCircle.css("left",Math.random()*200);
 		$newCircle.click(hide);
 		$newCircle.mouseenter(changeOpacity);
 		$newCircle.mouseleave(resetOpacity);
@@ -50,39 +54,44 @@ $(function(){
 	function grow(evt){
 			let growAmt=parseInt($('#growAmt').val());
 			
-		let circle=$('.circle');
+		let circles=$('.circle');
+
+if(countCirclePops===parseInt($('#number').val())){
+	$('#result').text('All Circles Poped in Time You win!!! Refresh page to load again!!!');
+	countCirclePops=-1;
+
+	clearInterval(timerId);
+}
+		for(let circle of circles) {
+
+			let newDiameter = parseInt($(circle).height()) + growAmt + "px";
+			let newLeft = parseInt($(circle).css("left")) - growAmt / 2 + "px";
+			let newTop = parseInt($(circle).css("top")) - growAmt / 2 + "px";
 
 
+			$(circle).css("width", newDiameter);
+			$(circle).css("height", newDiameter);
+			//$(circle).css("top",newTop);
+			//$(circle).css("left",newLeft);
 
-
-		let newDiameter=parseInt(circle.height())+growAmt+"px";
-		let newLeft=parseInt(circle.css("left"))-growAmt/2+"px";
-		let newTop=parseInt(circle.css("top"))-growAmt/2+"px";
-		
-		
-		$(circle).css("width",newDiameter);
-		$(circle).css("height",newDiameter);
-		//$(circle).css("top",newTop);
-		//$(circle).css("left",newLeft);
-		
-		
+		}
 		
 	}
 	
 	// $('.circle').click(function(evt){$(this).hide();});
 	$('.circle').click(hide);
 	
-	
+	let countCirclePops=0;
 function hide(evt){
 	//alert('hiding');
+	countCirclePops++;
 	$(this).css("visibility","hidden");
-	//$(this).hide();
+//	$(this).hide();
 
 	
 	
 }
 
-let timerForOpacity;
 function changeOpacity(evt) {
 
 	 timerForOpacity=setInterval((evt)=>{
@@ -114,9 +123,8 @@ function changeOpacity(evt) {
 }
 
 function resetOpacity(evt){
-	let t1=timerForOpacity;
 
-	clearInterval(t1);
+	clearInterval(timerForOpacity);
 	$(this).css("opacity","1");
 
 }
